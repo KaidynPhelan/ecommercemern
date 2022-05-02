61366e7545a3a173a1fd99eaae88646376098a4d
Kaidyn <kaidyn.phelan08@gmail.com>
2022-05-02 13:26:54 +1000

    initial commit react installed made header/main

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header classname="header">
+        <a href="/">amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
