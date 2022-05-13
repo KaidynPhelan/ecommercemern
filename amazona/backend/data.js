5dc8e794b1f0d15d4c467f043c9a29e4c19d7512
Kaidyn <kaidyn.phelan08@gmail.com>
2022-05-13 17:17:22 +1000

    made cart have quantitys of items

--- amazona/backend/data.js
+++ amazona/backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
+      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -25,6 +27,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pants',
       slug: 'nike-slim-pants',
       category: 'Pants',
@@ -37,6 +40,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
       name: 'Adidas Fit Pants',
       slug: 'addidas-fit-pants',
       category: 'Pants',
