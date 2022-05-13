4fde29ea9d0c923dd5e7615fe1bbd41a928c4f87
Kaidyn <kaidyn.phelan08@gmail.com>
2022-05-13 11:04:38 +1000

    used axios to fetch data for homescreen

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,13 +1,37 @@
 import { Link } from 'react-router-dom';
-import data from '../data';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await axios.get('/api/products/');
+  //     setProducts(result.data);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    FetchData();
+  });
+  async function FetchData() {
+    try {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       {' '}
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name}></img>
