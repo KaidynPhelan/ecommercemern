1c38beedbbc6a5579bb1d5a85f6fe8001c9b2b75
Kaidyn <kaidyn.phelan08@gmail.com>
2022-05-13 12:50:56 +1000

    added reducer hook to homescreen

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,28 +1,40 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 // import data from '../data';
 
-function HomeScreen() {
-  // const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios.get('/api/products/');
-  //     setProducts(result.data);
-  //   };
-  //   fetchData();
-  // }, []);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-  const [products, setProducts] = useState([]);
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     FetchData();
   }, []);
   async function FetchData() {
+    dispatch({ type: 'FETCH_REQUEST' });
     try {
       const response = await axios.get('/api/products');
-      setProducts(response.data);
+      // setProducts(response.data);
+      dispatch({ type: 'FETCH_SUCCESS', payload: response.data });
     } catch (error) {
-      console.log(error);
+      dispatch({ type: 'FETCH_FAIL', payload: error.message });
     }
   }
 
